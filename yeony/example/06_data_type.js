f026d8550d2e50bcc37242c1fa9a3e5f8dd13897
appleSimple <jy_4148@naver.com>
2022-09-20 00:17:07 +0900

    chapter 06

---
+++ yeony/example/06_data_type.js
@@ -0,0 +1,12 @@
+/* 6.1 숫자 타입 실습 */
+let binary = 0b01000001;
+let octal = 0o101;
+let hex = 0x41;
+
+console.log(binary); // 65
+console.log(octal); // 65
+console.log(hex); // 65
+
+console.log(10 / 0); // Infinity
+console.log(10 / -0); // -Infinity
+console.log(1 * 'string'); // NaN
